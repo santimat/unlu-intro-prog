9e13001cf5576590973964437e467211e969d4c5
santimat <santimaturo1@gmail.com>
2026-05-05 22:23:32 -0300

    agregando solución para la ecuación cuadrática

--- tp4/tp4-9.py
+++ tp4/tp4-9.py
@@ -1,1 +1,8 @@
-quadraticC
+a = float(input("Ingrese el valor para a: "))
+b = float(input("Ingrese el valor para b: "))
+c = float(input("Ingrese el valor para c: "))
+
+res = (-b + pow(pow(b,2) - 4*a*c,1/2)) / (2*a)
+res2 = (-b - pow(pow(b,2) - 4*a*c,1/2)) / (2*a)
+print(res)
+print(res2)
