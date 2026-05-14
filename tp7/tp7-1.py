e991cd1876a5b7ac396a788eea1a2327bd0b78a6
santimat <santimaturo1@gmail.com>
2026-05-14 16:25:39 -0300

    Agregar script para imprimir palabras hasta que se ingrese 'parar'

---
+++ tp7/tp7-1.py
@@ -0,0 +1,8 @@
+word = input("Ingrese una palabra\n> ")
+
+while (word != "parar"):
+  print(word)
+  word = input("Ingrese una nueva palabra\n> ")
+
+if(word == "parar"): print("======== TERMINADO ========")
+
