4f6d86be546b9ff91f35ad723cfcaa8953d5bdeb
santimat <santimaturo1@gmail.com>
2026-05-13 22:21:20 -0300

    Agregar script para determinar si un número es positivo, negativo o cero

---
+++ tp6/tp6-5.py
@@ -0,0 +1,9 @@
+import os
+for i in range(11):
+  number = int(input("Ingrese un numero: "))
+
+  if(number > 0): print(f"{number} es positivo")
+  elif(number < 0): print(f"{number} es negativo")
+  else: print(f"{number} es igual a 0")
+
+
